c6a33ffae027c887f0e01f80f987a6691b1a8816
cmerkens <cmerkens@spirit21.com>
2025-05-05 01:52:03 +0200

    Autogenerierter Code entfernt

--- EventTool/ET-Backend/Controllers/EventController.cs
+++ EventTool/ET-Backend/Controllers/EventController.cs
@@ -7,62 +7,10 @@ namespace ET_Backend.Controllers
     /// <summary>
     /// Bietet API-Endpunkte zur Verwaltung von Event-Daten.
     /// </summary>
-    [Route("api/[controller]")]
+    [Route("api/event")]
     [ApiController]
     public class EventController : ControllerBase
     {
-        /// <summary>
-        /// Ruft alle Event-Werte ab.
-        /// </summary>
-        /// <returns>Eine Liste von Beispiel-Events.</returns>
-        // GET: api/<EventController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        /// <summary>
-        /// Ruft ein bestimmtes Event anhand der ID ab.
-        /// </summary>
-        /// <param name="id">Die ID des Events.</param>
-        /// <returns>Das Event als Zeichenkette.</returns>
-        // GET api/<EventController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        /// <summary>
-        /// Erstellt ein neues Event.
-        /// </summary>
-        /// <param name="value">Der Event-Wert als Zeichenkette.</param>
-        // POST api/<EventController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        /// <summary>
-        /// Aktualisiert ein bestehendes Event anhand der ID.
-        /// </summary>
-        /// <param name="id">Die ID des zu aktualisierenden Events.</param>
-        /// <param name="value">Der neue Wert.</param>
-        // PUT api/<EventController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        /// <summary>
-        /// Löscht ein bestimmtes Event anhand der ID.
-        /// </summary>
-        /// <param name="id">Die ID des zu löschenden Events.</param>
-        // DELETE api/<EventController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
--- EventTool/ET-Backend/Controllers/UserController.cs
+++ EventTool/ET-Backend/Controllers/UserController.cs
@@ -7,62 +7,10 @@ namespace ET_Backend.Controllers
     /// <summary>
     /// Stellt API-Endpunkte zur Verwaltung von Benutzerdaten bereit.
     /// </summary>
-    [Route("api/[controller]")]
+    [Route("api/user")]
     [ApiController]
     public class UserController : ControllerBase
     {
-        /// <summary>
-        /// Ruft alle Benutzer oder Beispielwerte ab.
-        /// </summary>
-        /// <returns>Eine Liste von Beispiel-Benutzerdaten.</returns>
-        // GET: api/<UserController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        /// <summary>
-        /// Ruft einen bestimmten Benutzer anhand der ID ab.
-        /// </summary>
-        /// <param name="id">Die ID des Benutzers.</param>
-        /// <returns>Die Benutzerdaten als Zeichenkette.</returns>
-        // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        /// <summary>
-        /// Erstellt einen neuen Benutzer.
-        /// </summary>
-        /// <param name="value">Die Daten des neuen Benutzers.</param>
-        // POST api/<UserController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        /// <summary>
-        /// Aktualisiert einen bestehenden Benutzer anhand der ID.
-        /// </summary>
-        /// <param name="id">Die ID des zu aktualisierenden Benutzers.</param>
-        /// <param name="value">Die neuen Benutzerdaten.</param>
-        // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        /// <summary>
-        /// Löscht einen bestimmten Benutzer anhand der ID.
-        /// </summary>
-        /// <param name="id">Die ID des zu löschenden Benutzers.</param>
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
