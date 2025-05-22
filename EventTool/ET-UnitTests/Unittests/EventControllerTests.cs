f8be146267f8c9583ba2792ec4b2096ce3618561
Jonathan Kalmbach <i23018@hb.dhbw-stuttgart.de>
2025-05-22 15:21:12 +0200

    EventController/OrganisationController Unittests wieder angepasst, laufen alle durch

--- EventTool/ET-UnitTests/Unittests/EventControllerTests.cs
+++ EventTool/ET-UnitTests/Unittests/EventControllerTests.cs
@@ -8,69 +8,28 @@ using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ET_Backend.Services.Person;
+using System.Security.Claims;
 
 namespace ET_UnitTests.Unittests
 {
     public class EventControllerTests
     {
-        /*
-        [Fact]
-        public async Task EventList_ReturnsOk_WithEventListDtos()
-        {
-            // Arrange
-            var mockService = new Mock<IEventService>();
-            var user = new Account { Id = 1, Organization = new Organization { Id = 2 } };
-            var events = new List<Event>
-            {
-                new Event
-                {
-                    Id = 10,
-                    Name = "TestEvent",
-                    Description = "Desc",
-                    MaxParticipants = 100,
-                    Organizers = new List<Account> { user },
-                    Participants = new List<Account> { user },
-                    Organization = user.Organization
-                }
-            };
-            mockService.Setup(s => s.GetEventsFromOrganization(user.Organization.Id))
-                .ReturnsAsync(Result.Ok(events));
+        private static Account GetTestUser() =>
+            new Account { Id = 1, Organization = new Organization { Id = 2 } };
 
-            var controller = new EventController(mockService.Object);
-
-            // Act
-            var result = await controller.EventList();
-
-            // Assert
-            var okResult = Assert.IsType<OkObjectResult>(result);
-            var dtos = Assert.IsAssignableFrom<List<EventListDto>>(okResult.Value);
-            Assert.Single(dtos);
-            Assert.Equal("TestEvent", dtos[0].Name);
-        }
-
-    
-        [Fact]
-        public async Task EventList_ReturnsBadRequest_OnFailure()
-        {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.GetEventsFromOrganization(It.IsAny<int>()))
-                .ReturnsAsync(Result.Fail("Fehler"));
-
-            var controller = new EventController(mockService.Object);
-
-            var result = await controller.EventList();
-
-            Assert.IsType<BadRequestResult>(result);
-        }
-        */
         [Fact]
         public async Task SubscribeToEvent_ReturnsOk_OnSuccess()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.SubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.SubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
                 .ReturnsAsync(Result.Ok());
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var result = await controller.SubscribeToEvent(5);
 
@@ -80,11 +39,15 @@ namespace ET_UnitTests.Unittests
         [Fact]
         public async Task SubscribeToEvent_ReturnsBadRequest_OnFailure()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.SubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.SubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
                 .ReturnsAsync(Result.Fail("Fehler"));
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var result = await controller.SubscribeToEvent(5);
 
@@ -94,11 +57,15 @@ namespace ET_UnitTests.Unittests
         [Fact]
         public async Task UnsubscribeToEvent_ReturnsOk_OnSuccess()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.UnsubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.UnsubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
                 .ReturnsAsync(Result.Ok());
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var result = await controller.UnsubscribeToEvent(5);
 
@@ -108,11 +75,15 @@ namespace ET_UnitTests.Unittests
         [Fact]
         public async Task UnsubscribeToEvent_ReturnsBadRequest_OnFailure()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.UnsubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.UnsubscribeToEvent(It.IsAny<int>(), It.IsAny<int>()))
                 .ReturnsAsync(Result.Fail("Fehler"));
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var result = await controller.UnsubscribeToEvent(5);
 
@@ -122,20 +93,24 @@ namespace ET_UnitTests.Unittests
         [Fact]
         public async Task CreateEvent_ReturnsOk_OnSuccess()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.CreateEvent(It.IsAny<Event>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.CreateEvent(It.IsAny<Event>()))
                 .ReturnsAsync(Result.Ok(new Event()));
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var dto = new EventDto(
-                "Test",             
-                "Desc",            
-                DateOnly.Parse("2025-06-01"), 
-                DateOnly.Parse("2025-06-01"), 
-                5,                
-                20,                
-                false               
+                "Test",
+                "Desc",
+                DateOnly.Parse("2025-06-01"),
+                DateOnly.Parse("2025-06-01"),
+                5,
+                20,
+                false
             );
 
             var result = await controller.CreateEvent(dto);
@@ -143,40 +118,18 @@ namespace ET_UnitTests.Unittests
             Assert.IsType<OkResult>(result);
         }
 
-        /*
-        [Fact]
-        public async Task CreateEvent_ReturnsBadRequest_OnFailure()
-        {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.CreateEvent(It.IsAny<Event>()))
-                .ReturnsAsync(Result.Fail("Fehler"));
-
-            var controller = new EventController(mockService.Object);
-
-            var dto = new EventDto(
-                "Test",             // Name
-                "Desc",             // Description
-                DateOnly.Parse("2025-06-01"), // StartDate
-                DateOnly.Parse("2025-06-01"), // EndDate
-                5,                  // MinParticipants
-                20,                 // MaxParticipants
-                false               // IsBlueprint
-            );
-
-            var result = await controller.CreateEvent(dto);
-
-            Assert.IsType<BadRequestResult>(result);
-        }
-        */
-
         [Fact]
         public async Task DeleteEvent_ReturnsOk_OnSuccess()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.DeleteEvent(It.IsAny<int>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.DeleteEvent(It.IsAny<int>()))
                 .ReturnsAsync(Result.Ok());
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var result = await controller.DeleteEvent(7);
 
@@ -186,11 +139,15 @@ namespace ET_UnitTests.Unittests
         [Fact]
         public async Task DeleteEvent_ReturnsBadRequest_OnFailure()
         {
-            var mockService = new Mock<IEventService>();
-            mockService.Setup(s => s.DeleteEvent(It.IsAny<int>()))
+            var mockEventService = new Mock<IEventService>();
+            mockEventService.Setup(s => s.DeleteEvent(It.IsAny<int>()))
                 .ReturnsAsync(Result.Fail("Fehler"));
 
-            var controller = new EventController(mockService.Object);
+            var mockUserService = new Mock<IUserService>();
+            mockUserService.Setup(s => s.GetCurrentUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .ReturnsAsync(GetTestUser());
+
+            var controller = new EventController(mockEventService.Object, mockUserService.Object);
 
             var result = await controller.DeleteEvent(7);
 
