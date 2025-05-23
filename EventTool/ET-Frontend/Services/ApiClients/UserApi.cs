f071504a7b3a9744459826dd8a537880be39f707
Tobias Schuhmacher <i23036@hb.dhbw-stuttgart.de>
2025-05-24 00:29:40 +0200

    5.1 FIX AuthenticationStateProvider im ApiClient vergessen...

--- EventTool/ET-Frontend/Services/ApiClients/UserApi.cs
+++ EventTool/ET-Frontend/Services/ApiClients/UserApi.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using ET_Frontend.Helpers;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace ET_Frontend.Services.ApiClients
 {
@@ -12,26 +13,28 @@ namespace ET_Frontend.Services.ApiClients
     public class UserApi : IUserApi
     {
         private readonly HttpClient _httpClient;
+        private readonly AuthenticationStateProvider _authProvider;
 
         /// <summary>
-        /// Konstruktor mit HttpClient-Injektion.
+        /// Konstruktor mit HttpClient- und AuthenticationStateProvider-Injektion.
         /// </summary>
-        public UserApi(HttpClient httpClient)
+        public UserApi(HttpClient httpClient, AuthenticationStateProvider authProvider)
         {
             _httpClient = httpClient;
+            _authProvider = authProvider;
         }
 
         /// <inheritdoc />
         public async Task<UserEditViewModel?> GetCurrentUserAsync()
         {
-            var userId = JwtClaimHelper.GetUserIdAsync();
+            var userId = JwtClaimHelper.GetUserIdAsync(_authProvider);
             return await _httpClient.GetFromJsonAsync<UserEditViewModel>($"user/{userId}");
         }
 
         /// <inheritdoc />
         public async Task<bool> UpdateUserAsync(UserEditViewModel model)
         {
-            var userId = JwtClaimHelper.GetUserIdAsync();
+            var userId = JwtClaimHelper.GetUserIdAsync(_authProvider);
             var response = await _httpClient.PutAsJsonAsync($"user/{userId}", model);
             return response.IsSuccessStatusCode;
         }
